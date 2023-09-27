cbdb691a8d9c372687265a053974e8339d1388f5
Саша <epra11111@mail.ru>
2023-09-27 13:12:56 +0400

    Add URL and open gismeteo

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
-URL = "https://www.gismeteo.ru"
+Url = "https://www.gismeteo.ru/diary/4368/2023/9/"
 headers = {
 "Accept":"*/*",
-"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 OPR/102.0.0.0 (Edition Yx GX)"
+"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 OPR/102.0.0.0 (Edition Yx GX)"
 }
-req = requests.get(URL,headers = headers)
+req = requests.get(Url,headers = headers)
 src = req.text
+with open("index.html","w")as file:
+    file.write(src)
 print(src)
+# with open("index.hutml") as file:
+#     src = file.read()
+# soup = BeautifulSoup(src,"lxml")
+# all_days = soup.find_all(class_ ="current-time")
+# for item in all_days:
+#     item_text = item.text
+#     print(item_text)
