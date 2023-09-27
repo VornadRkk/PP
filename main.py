26064fdfa7dee3e767f46322c81fdd9e3f9a9245
Саша <epra11111@mail.ru>
2023-09-27 17:37:36 +0400

    Final 1.0

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from bs4 import BeautifulSoup
 import csv
+import re
 import requests
-# Url = "https://www.gismeteo.ru/diary/4618/2023/9/"
-# headers = {
-# "Accept":"*/*",
-# "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 OPR/102.0.0.0 (Edition Yx GX)"
-# }
-# req = requests.get(Url,headers = headers)
-# src = req.text
-with open("index.html")as file:
-    src = file.read()
+Url = "https://www.gismeteo.ru/diary/4618/2023/9/"
+headers = {
+"Accept":"*/*",
+"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 OPR/102.0.0.0 (Edition Yx GX)"
+}
+req = requests.get(Url,headers = headers)
+src = req.text
+with open("index.html", "w") as file:
+    file.write(src)
+# with open("index.html")as file:
+#     src = file.read()
 soup = BeautifulSoup(src,"lxml")
 table = soup.find("table").find("tbody").find_all("tr")
 data_table = []
 for item in table:
     data_td = item.find_all('td')
-    day = data_td[0].text+soup.find("div",class_="header").text
+    day = data_td[0].text
     temp_morning = data_td[1].text
     presure_morning = data_td[2].text
     wind_morning = data_td[5].text
@@ -25,7 +28,7 @@ for item in table:
 
     data_table.append(
         {
-            "day": day,
+            "day": day + ".09.2023",
             "temp_morinng": temp_morning,
             "presure_morning": presure_morning,
             "wind_morning": wind_morning,
