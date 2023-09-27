1478f3f9a0088fe6629d0743b99413300a26dbe0
Саша <epra11111@mail.ru>
2023-09-27 11:42:27 +0400

    Add URL

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 from bs4 import BeautifulSoup
-2222
+import requests
+URL = "https://www.gismeteo.ru"
+headers = {
+"Accept":"*/*",
+"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 OPR/102.0.0.0 (Edition Yx GX)"
+}
+req = requests.get(URL,headers = headers)
+src = req.text
+print(src)
